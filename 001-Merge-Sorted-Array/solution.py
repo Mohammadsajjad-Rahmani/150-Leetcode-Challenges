623b6f6d2c9f3ba6e5551497e84af78323e4e806
Mohammadsajjad <Mohammadsajjadmail@gmail.com>
2025-04-22 10:31:59 +0330

    Update solution ♻

--- 001-Merge-Sorted-Array/solution.py
+++ 001-Merge-Sorted-Array/solution.py
@@ -6,11 +6,20 @@ def merge_sorted_arrays(nums1, m, nums2, n):
     while i >= 0 and j >= 0:
         if nums1[i] > nums2[j]:
             nums1[k] = nums1[i]
+            k -= 1
             i -= 1
-        else:
+        elif nums2[j] > nums1[i]:
             nums1[k] = nums2[j]
             j -= 1
-        k -= 1
+            k -= 1  
+        else :
+            nums1[k] = nums1[i]
+            i -= 1
+            k -= 1
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
 
     while j >= 0:
         nums1[k] = nums2[j]
